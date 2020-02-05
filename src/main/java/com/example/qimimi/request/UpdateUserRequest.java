9d0766fc6631f8ad89e70a79413273c5389c41ce
a1249279630 <1249279630@qq.com>
2020-02-05 19:24:18 +0800

    12

---
+++ src/main/java/com/example/qimimi/request/UpdateUserRequest.java
@@ -0,0 +1,15 @@
+package com.example.qimimi.request;
+
+public class UpdateUserRequest {
+    private Long phone;
+
+    private String email;
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
